74ab636dab4c8a040da46240815f35cb82a8d1d6
Poorani R <poorani.r0405@gmail.com>
2026-06-18 12:52:43 +0530

    Fixed mobile navbar and responsive design

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ document.querySelectorAll(
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
+if(menuToggle){
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
